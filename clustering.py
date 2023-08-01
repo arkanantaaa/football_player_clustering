5e313958a11a2062e6bff3714cb217be51f45440
arkanantaaa <97844761+arkanantaaa@users.noreply.github.com>
2023-08-01 08:57:42 +0700

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -16,16 +16,12 @@ st.markdown(
   unsafe_allow_html=True
 )
 
-dt = pd.read_csv('dt.csv')
-# dt_forward = pd.read_csv('dt_forward.csv')
-# dt_attack = pd.read_csv('dt_attack.csv')
-# dt_winger = pd.read_csv('dt_winger.csv')
-# dt_midfielder = pd.read_csv('dt_midfielder.csv')
-# dt_fullback = pd.read_csv('dt_fullback.csv')
-# dt_CB = pd.read_csv('dt_CB.csv')
-
-# unique_pos = ['Forward', 'Attacking 10', 'Midfielder', 'Winger', 'Fullback', 'Center Back']
-# selected_pos = st.selectbox('Posisi', unique_pos, unique_pos)
+df_CF = pd.read_excel('df_CF.xlsx')
+df_CB = pd.read_excel('df_CB.xlsx')
+df_AM = pd.read_excel('df_AM.xlsx')
+df_CM = pd.read_excel('df_CM.xlsx')
+df_Sideback = pd.read_excel('df_Sideback.xlsx')
+df_Winger = pd.read_excel('df_Winger.xlsx')
 
 # Preprocessing data
 data_encoded = pd.get_dummies(dt.drop(['Name'], axis=1))
@@ -38,31 +34,25 @@ def kmeans_clustering(data, n_clusters):
 
 # Sidebar untuk memilih posisi pemain dan jumlah klaster
 st.sidebar.header('Pilih posisi pemain yang ingin Diklasterisasi')
-posisi_pemain = st.sidebar.selectbox('Posisi Pemain', dt['Position'].unique())
+dataframe_choice = st.sidebar.selectbox("Pilih Posisi Pemain", ['CF', 'CB', 'AM', 'CM', 'Sideback', 'Winger'])
 st.sidebar.subheader('Pilih jumlah klaster yang diinginkan')
 jumlah_klaster = st.sidebar.slider('Jumlah Klaster', min_value=2, max_value=5, value=3, step=1)
 
-# Filter data berdasarkan posisi pemain yang dipilih
-filtered_data = dt[dt['Position'] == posisi_pemain]
-
-# Fitur-fitur yang sesuai dengan posisi pemain
-fitur_per_posisi = {
-    'Forward': ['Shots Total', 'Goal', 'Create Chance', 'Shot On%', 'Conversion Ratio', 'Assist', 'Pass Accuracy', 'Dribble', 'Tackle', 'Intercept', 'Recovery', 'Aerial Won%'],
-    'Attacking 10': ['Shots Total', 'Goal', 'Create Chance', 'Shot On%', 'Conversion Ratio', 'Assist', 'Pass Accuracy', 'Dribble', 'Cross', 'Tackle', 'Intercept', 'Recovery'],
-    'Winger': ['Shots Total', 'Goal', 'Create Chance', 'Shot On%', 'Conversion Ratio', 'Assist', 'Pass Accuracy', 'Dribble', 'Cross', 'Tackle', 'Intercept', 'Recovery'],
-    'Midfielder': ['Shots Total', 'Goal', 'Create Chance', 'Shot On%', 'Conversion Ratio', 'Assist', 'Pass Accuracy', 'Dribble', 'Tackle', 'Intercept', 'Recovery', 'Block'],
-    'Fullback': ['Shots Total', 'Goal', 'Create Chance', 'Assist', 'Pass Accuracy', 'Dribble', 'Cross', 'Tackle', 'Intercept', 'Recovery', 'Block', 'Aerial Won%'],
-    'Center Back': ['Shots Total', 'Goal', 'Assist', 'Pass Accuracy', 'Tackle', 'Intercept', 'Recovery', 'Block', 'Aerial Won%']
-}
-
-# Memilih fitur sesuai dengan posisi pemain
-fitur = fitur_per_posisi[posisi_pemain]
-
-# Filter data berdasarkan fitur yang dipilih
-filtered_data = filtered_data[['Name', 'Position'] + fitur]
+if dataframe_choice == 'CF':
+    df = df_CF
+elif dataframe_choice == 'CB':
+    df = df_CB
+elif dataframe_choice == 'AM':
+    df = df_AM
+elif dataframe_choice == 'CM':
+    df = df_CM
+elif dataframe_choice == 'Sideback':
+    df = df_Sideback
+else: 
+    df = df_Winger
 
 # Klasterisasi
-labels = kmeans_clustering(filtered_data.drop(['Name', 'Position'], axis=1), jumlah_klaster)
+labels = kmeans_clustering(df.drop(['Name', 'Position'], axis=1), jumlah_klaster)
 
 # Menampilkan hasil klasterisasi
 st_title = '<p style="font-family:sans-serif ;font-weight:800; color:#FFD700; font-size: 50px;">Klasterisasi Pemain Sepak Bola Berdasarkan Posisi</p>'
@@ -78,14 +68,14 @@ if is_note_clicked:
 
 
 # Tampilkan tabel dengan data pemain dan klaster
-filtered_data['Klaster'] = labels
-st.write(filtered_data)
+df['Klaster'] = labels
+st.write(df)
 
 # Visualisasi hasil klasterisasi
 fig, ax = plt.subplots()
-scatter = ax.scatter(filtered_data[fitur[0]], filtered_data[fitur[1]], c=labels, cmap='rainbow')
-ax.set_xlabel(fitur[0])
-ax.set_ylabel(fitur[1])
+scatter = ax.scatter(df['Name'], df['MoP'], c=labels, cmap='rainbow')
+ax.set_xlabel(df['Name'])
+ax.set_ylabel(df['MoP'])
 ax.set_title('Klasterisasi Pemain Sepak Bola Berdasarkan Posisi')
 ax.legend(*scatter.legend_elements(), title='Klaster')
 st.pyplot(fig)
@@ -103,5 +93,5 @@ def filedownload(df):
 if download_btn:
     st.write('')
     st.subheader('Download Hasil Klasterisasi')
-    st.markdown(filedownload(filtered_data), unsafe_allow_html=True)
+    st.markdown(filedownload(df), unsafe_allow_html=True)
 
